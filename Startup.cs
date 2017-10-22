152747796df0020c548264be6f6d5c4db9ae3bc4
cmsjr <charles.sellers@gmail.cim>
2017-10-21 22:19:06 -0700

    adding dotnetcore sample

---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+
+namespace hwapp
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Run(async (context) => { await context.Response.WriteAsync("Hello, world!"); });
+        }
+    }
+}
